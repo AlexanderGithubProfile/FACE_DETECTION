c073b023b64b641fc6bf216b018519aaa2b0ad55
Teh Inc <tehpromconnection@gmail.com>
2023-01-23 13:59:28 +0000

    Update Data_maining.py

--- Data_maining.py
+++ Data_maining.py
@@ -1,8 +1,8 @@
 import pandas as pd
-def get_dataframe_dtypes(f1le_path):
-    df = pd.read_csv(tJle_path)
+def get_dataframe_dtypes(file_path):
+    df = pd.read_csv(file_path)
     return df.dtypes
 
 def get_dataframe_shape(f1le_path):
-    df = pd.read_csv(tJle_path)
+    df = pd.read_csv(file_path)
     return df.shape
