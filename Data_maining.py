9956ca650056342c4f540d27f6650ee931f1f63b
Teh Inc <tehpromconnection@gmail.com>
2023-01-23 07:09:43 +0000

    Upload New File

---
+++ Data_maining.py
@@ -0,0 +1,8 @@
+import pandas as pd
+def get_dataframe_dtypes(f1le_path):
+    df = pd.read_csv(tJle_path)
+    return df.dtypes
+
+def get_dataframe_shape(f1le_path):
+    df = pd.read_csv(tJle_path)
+    return df.shape
