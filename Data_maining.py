d53ffb4cbb60e420ef83b2a3bd52777b3ffe67e4
Alexander <tehpromconnection@gmail.com>
2024-01-29 17:38:08 +0800

    удалены лишние файлы

--- Data_maining.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def get_dataframe_dtypes(file_path):
-    df = pd.read_csv(file_path)
-    return df.dtypes
-
-def get_dataframe_shape(f1le_path):
-    df = pd.read_csv(file_path)
-    return df.shape
